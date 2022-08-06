ce6cdf350908ad8c90f1e5604e6db31260a31dc4
firdavs0707 <firdavs0.4.2.0.0.7>
2022-08-06 12:34:07 +0500

    add codes

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ var elCheck = document.querySelector('.checkbox');
 elForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
+  if (elInput.value == 0) {
+    elResult.textContent = 'Kocha haroratini kiriting!';
+    return;
+  }
   if (isNaN(elInput)) {
     elResult.textContent = 'Son kiriting!';
   }
-  if (elInput.value == 0) {
-    elResult.textContent = 'Kocha haroratini kiriting!';
+  if (elCheck.checked == 1) {
+    elResult.textContent = 'Yugurishga ertaga chiqishingizni maslahat Beramiz!';
+    return;
   }
   if ((elInput.value < 5) || (elInput.value > 40)) {
     elResult.textContent = 'Yugurishga ertaga chiqishingizni maslahat Beramiz!';
   }
   if ((elInput.value >= 5) && (elInput.value <= 40)) {
     elResult.textContent = 'Kochada yugurishingiz mumkin!';
-  }
-  if (elCheck.checked == 1) {
-    elResult.textContent = 'Yugurishga ertaga chiqishingizni maslahat Beramiz!';
   }
 })
 
